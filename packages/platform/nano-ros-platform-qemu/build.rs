2d90ee9bd536ae65f1c5dc16a425fcd5266b7b54
aeon <jerry73204@gmail.com>
2026-02-14 08:49:15 +0800

    Add mps2-an385.x linker script back

---
+++ packages/platform/nano-ros-platform-qemu/build.rs
@@ -0,0 +1,23 @@
+//! Build script for nano-ros-platform-qemu
+//!
+//! Copies the mps2-an385.x linker script to the output directory as memory.x
+//! so that cortex-m-rt can find it during linking.
+
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    File::create(out.join("memory.x"))
+        .unwrap()
+        .write_all(include_bytes!("mps2-an385.x"))
+        .unwrap();
+
+    println!("cargo:rustc-link-search={}", out.display());
+
+    println!("cargo:rerun-if-changed=mps2-an385.x");
+    println!("cargo:rerun-if-changed=build.rs");
+}
